a2c59e0b0fbfd5464d1d6e2e4acd8e5485b49f65
irayspace <irayspacii@gmail.com>
2022-04-28 14:47:37 +0800

    feat: initial texture

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use image::GenericImageView;
 use rand::prelude::*;
 use wgpu::util::DeviceExt;
 use winit::{
@@ -182,6 +183,42 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
         contents: bytemuck::cast_slice(HEX_INDICES),
         usage: wgpu::BufferUsages::INDEX,
     });
+
+    let diffuse_bytes = include_bytes!("kakashi.png");
+    let diffuse_image = image::load_from_memory(diffuse_bytes).unwrap();
+    let diffuse_rgba = diffuse_image.to_rgba8();
+    let dimensions = diffuse_image.dimensions();
+    let texture_size = wgpu::Extent3d {
+        width: dimensions.0,
+        height: dimensions.1,
+        depth_or_array_layers: 1,
+    };
+
+    let diffuse_texture = device.create_texture(&wgpu::TextureDescriptor {
+        size: texture_size,
+        mip_level_count: 1,
+        sample_count: 1,
+        dimension: wgpu::TextureDimension::D2,
+        format: wgpu::TextureFormat::Rgba8UnormSrgb,
+        usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
+        label: Some("diffuse_texture"),
+    });
+
+    queue.write_texture(
+        wgpu::ImageCopyTexture {
+            texture: &diffuse_texture,
+            mip_level: 0,
+            origin: wgpu::Origin3d::ZERO,
+            aspect: wgpu::TextureAspect::All,
+        },
+        &diffuse_rgba,
+        wgpu::ImageDataLayout {
+            offset: 0,
+            bytes_per_row: std::num::NonZeroU32::new(4 * dimensions.0),
+            rows_per_image: std::num::NonZeroU32::new(dimensions.1),
+        },
+        texture_size,
+    );
 
     let mut color = wgpu::Color {
         r: 1.0,
